7c3b7ac185f928be24e552aada62706567bfa31d
Diogo Carapito <diogo.carapito@icloud.com>
2022-11-02 16:50:19 +0000

    .

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import dash_bootstrap_components as dbc
 from dash import Input, Output, State, html, dcc
 import pandas as pd
 
-df = pd.read_excel('data.xlsx', header=2)
-
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SIMPLEX])
 #app = dash.Dash(__name__, use_pages=True)
 
