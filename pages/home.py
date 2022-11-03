57df55a82f7ea7b117c5c8fd40d972b3c584392c
Diogo Carapito <diogo.carapito@icloud.com>
2022-11-03 03:08:38 +0000

    .

--- pages/home.py
+++ pages/home.py
@@ -12,8 +12,8 @@ dash.register_page(__name__,
 df = pd.read_csv('scrapped_indicadores.csv')
 df.drop(columns=df.columns[0], axis=1, inplace=True)
 
-#table = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
-table = dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=True)
+table = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+#table = dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=True)
 
 container_1 = dbc.Container([
     html.H3('Dashboard'),
