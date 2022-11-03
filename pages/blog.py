4d39c7101f83790d4735dc341ff873aa968b0d6c
Diogo Carapito <diogo.carapito@icloud.com>
2022-11-03 03:32:48 +0000

    .

--- pages/blog.py
+++ pages/blog.py
@@ -1,14 +1,22 @@
 import dash
 from dash import dcc, html
 import dash_bootstrap_components as dbc
+import pandas as pd
+import csv
 
 dash.register_page(__name__,
                    path='/blog',
                    title='Blog',
                    name='Blog')
 
+file = open("scrapped_indicadores.csv", "r")
+dados = list(csv.reader(file, delimiter=","))
+df = pd.DataFrame(dados)
+table = dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=False)
+
 container_1 = dbc.Container([
     html.H3('blog'),
+    table
     ])
 
 layout = html.Div([
--- pages/home.py
+++ pages/home.py
@@ -9,7 +9,7 @@ dash.register_page(__name__,
                    name='Dashboard'
                    )
 
-df = pd.read_csv('scrapped_indicadores.csv')
+#df = pd.read_csv('scrapped_indicadores.csv')
 '''df.drop(columns=df.columns[0], axis=1, inplace=True)
 
 #table = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
