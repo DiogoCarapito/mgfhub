6964e79e6569f3006935eb3b164ed23d3ed9fa27
Diogo Carapito <diogo.carapito@icloud.com>
2022-11-08 18:25:58 +0000

    .

--- pages/blog.py
+++ pages/blog.py
@@ -9,29 +9,30 @@ dash.register_page(
     name='blog',
     order=4,
 )
+posts_html = str()
+blogposts = [['first post','4/11/2022','text text'],['second post','8/11/2022','text text2']]
+for each in blogposts:
+    post = html.Div(dbc.Container([
+                html.H2(each[0]),
+                html.P(each[1]),
+                html.Hr(className="my-2"),
+                html.P(each[2]),
+            ],fluid=True, className="py-3",
+        ),className="p-3 bg-light rounded-3",
+    ),
 
-blogposts = [['4/11/2022','first post','text text'],[]]
-
+'''
 jumbotron = html.Div(
     dbc.Container(
         [
-            html.H4("Jumbotron", className="display-3"),
-            html.P(
-                "Use Containers to create a jumbotron to call attention to "
-                "featured content or information.",
-                className="lead",
-            ),
+            html.H2(blogposts[0][0]),
+            html.P(blogposts[0][1]),
             html.Hr(className="my-2"),
-            html.P(
-                "Use utility classes for typography and spacing to suit the "
-                "larger container."
-            ),
-        ],
-        fluid=True,
-        className="py-3",
-    ),
-    className="p-3 bg-light rounded-3",
+            html.P(blogposts[0][2]),
+        ],fluid=True, className="py-3",
+    ),className="p-3 bg-light rounded-3",
 )
+'''
 
 blog = dbc.Container([
     html.H3('blog'),
@@ -45,6 +46,6 @@ container_1 = dbc.Container([
 
 def layout():
     return html.Div([
-        jumbotron,
+        posts_html,
         html.Br(),
     ])
