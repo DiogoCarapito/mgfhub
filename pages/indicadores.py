89ca82e923b2c517cd02d7ade1739b595186cb0b
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-05-03 01:17:22 +0100

    merge of tabular and card like vres into the same page, using tabs from bootstrap

--- pages/indicadores.py
+++ pages/indicadores.py
@@ -13,7 +13,10 @@ dash.register_page(
     order=2,
 )
 
-
+# definir cores
+cor_vermelha = '#CB707A'
+cor_laranja = '#E6C68F'
+cor_verde = '#86BF8B'
 
 # Laod da principal base de dados em .csv
 ## Ponderar fazer um script para adicionar informação sobre Intervalos aceitáveis ideias a partir do PDF da Operacionalização da ACSS
@@ -25,6 +28,96 @@ df_todos_indicadores = pd.read_csv('data/indicadores_post_processed.csv')
 ## eventualmente tem que se escrever um script para extrair diretamente do PDF da Operacionalização da ACSS
 usf_ucsp_para_idg = pd.read_csv('data/usf_ucsp_indicadores_2022_comimpactoIDG.csv')
 usf_ucsp_sem_idg = pd.read_csv('data/usf_ucsp_indicadores_2022_semimpactoIDG.csv')
+
+
+# definição de cartão
+def generate_html_indicador(indicador):
+    separador = ' - '
+    return dbc.Container([
+        dbc.Card([
+            dbc.CardHeader(children=html.H5([str(indicador[1][0]) + separador + indicador[1][1]])),
+            dbc.CardBody([
+                dbc.Container([
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.Row([
+                                html.B([str(indicador[1]['area']) + ' > ' + str(indicador[1]['subarea']) + ' > ' + str(indicador[1]['dimensao'])])
+                            ]),
+                            html.Br(),
+                            dbc.Row([
+                                html.P([indicador[1]['objetivo']]),
+                                html.A(indicador[1]['link'], href=indicador[1]['link']),
+                            ]),
+
+                        ],width=7),
+                        dbc.Col([],width=1),
+                        dbc.Col([
+                            dbc.Row([
+                                dbc.Progress([
+                                    dbc.Progress(
+                                        id=str(indicador[1][0]) + '_0-',
+                                        value=indicador[1]['min_aceitavel'],
+                                        color=cor_vermelha,
+                                        bar=True),
+                                    dbc.Progress(
+                                        id=str(indicador[1][0]) + '_1-',
+                                        value=indicador[1]['min_esperado'] - indicador[1]['min_aceitavel'],
+                                        color=cor_laranja,
+                                        bar=True),
+                                    dbc.Progress(
+                                        id=str(indicador[1][0]) + '_2',
+                                        value=indicador[1]['max_esperado'] - indicador[1]['min_esperado'],
+                                        color=cor_verde,
+                                        bar=True),
+                                    dbc.Progress(
+                                        id=str(indicador[1][0]) + '_1+',
+                                        value=indicador[1]['max_aceitavel'] - indicador[1]['max_esperado'],
+                                        color=cor_laranja,
+                                        bar=True,
+                                        #striped=True
+                                    ),
+                                    dbc.Progress(
+                                        id=str(indicador[1][0]) + '_0+',
+                                        value=100 - indicador[1]['max_aceitavel'],
+                                        color=cor_vermelha,
+                                        bar=True,
+                                        #striped=True
+                                    ),
+                                ], style={"height": "20px"}),
+                                dbc.Tooltip(
+                                    'score 0- ' + '[0 - ' + str(indicador[1]['min_aceitavel']) + ']',
+                                    target=str(indicador[1][0]) + '_0-',
+                                    placement='top'),
+                                dbc.Tooltip(
+                                    'score 1- [' + str(indicador[1]['min_aceitavel']) + ' - ' + str(indicador[1]['min_esperado']) + ']',
+                                    target=str(indicador[1][0]) + '_1-',
+                                    placement='top'),
+                                dbc.Tooltip(
+                                    'score 2 [' + str(indicador[1]['min_esperado']) + ' - ' + str(indicador[1]['max_esperado']) + ']',
+                                    target=str(indicador[1][0]) + '_2',
+                                    placement='top'),
+                                dbc.Tooltip(
+                                    'score 1+ [' + str(indicador[1]['max_esperado']) + ' - ' + str(indicador[1]['max_aceitavel']) + ']',
+                                    target=str(indicador[1][0]) + '_1+',
+                                    placement='top'),
+                                dbc.Tooltip(
+                                    'score 0+ [' + str(indicador[1]['max_aceitavel']) + ' - 100' + ']',
+                                    target=str(indicador[1][0]) + '_0+',
+                                    placement='top'),
+                            ]),
+                            html.Br(),
+                            dbc.Row([
+                                html.P(['intervalo aceitável: ' + str(indicador[1]['intervalo_aceitavel'])], style={'color':cor_laranja}),
+                                html.P(['intervalo esperado: ' + str(indicador[1]['intervalo_esperado'])], style={'color':cor_verde}),
+                            ]),
+                        ], width=4),
+                    ]),
+
+                ]),
+            ]),
+        ],class_name="bg-light"),
+        #html.Br(),
+    ], fluid=True, class_name="py-3")
 
 
 # Definição da Tabela
@@ -52,7 +145,7 @@ table = dash_table.DataTable(
         },
     style_data={'whiteSpace': 'normal','height': 'auto',},
     style_table={'overflowX': 'auto'},
-
+    editable=True,
     # Linhas com fundo alternado
     style_data_conditional=[
         {
@@ -68,7 +161,7 @@ table = dash_table.DataTable(
 search_box = html.Div([
     dbc.Input(
         id='searchbox',
-        placeholder='search...',
+        placeholder='procurar...',
         type='text',
         size='sm',
     ),
@@ -105,15 +198,34 @@ filters = html.Div([
 # Contagem de quantos indicadores estão selecionados
 contagem = html.Div([html.P(id='searchbox_indicadores_tabela')],style={'padding': '12px 0px 0px 4px'})
 
+cartoes_indicadores = html.Div([],id='cartoes_indicadores')
+
+tab1_tabela = dbc.Card(
+    dbc.CardBody([
+        table
+    ]),
+)
+#,style={"background-color": "#ADD8E6"}
+tab2_cartões = dbc.Card(
+    dbc.CardBody([
+        cartoes_indicadores
+    ],className="p-0"),
+)
+
+tabs = dbc.Tabs([
+    dbc.Tab(tab1_tabela, label="tabela"),
+    dbc.Tab(tab2_cartões, label="cartões"),
+])
+
 # Contentor principal
 container_1 = dbc.Container([
     dbc.Row([
         dbc.Col([
-            html.H3('lista de indicadores'),
+            html.H3('indicadores'),
             html.Br(),
             filters,
             contagem,
-            table,
+            tabs,
         ])
     ])
 ], fluid=True)
@@ -129,6 +241,7 @@ def layout():
 @callback(
     Output('tabela_indicadores', 'data'),
     Output('tabela_indicadores', 'columns'),
+    Output('cartoes_indicadores', 'children'),
     Output('searchbox_indicadores_tabela','children'),
     Input('searchbox', 'value'),
     Input('radio_tabela', 'value'),
@@ -167,7 +280,9 @@ def table_update(searchbox,radio_tabela):
         '''
 
     # remoção das colunas que não se querem vizualizar
-    df_after_search = df_after_search.drop(
+    df_after_search_tabela = df_after_search.copy()
+
+    df_after_search_tabela = df_after_search_tabela.drop(
         columns=[
             'id',
             'codigo',
@@ -193,16 +308,43 @@ def table_update(searchbox,radio_tabela):
     )
 
 
-    df_id_sdm = df_after_search.pop('id_sdm')
-    df_after_search.insert(0, 'id', df_id_sdm)
+    df_id_sdm = df_after_search_tabela.pop('id_sdm')
+    df_after_search_tabela.insert(0, 'id', df_id_sdm)
 
     # Duas variáveis necessárias à exportação da tabela
-    df_data = df_after_search.to_dict(orient='records')
-    df_columns = [{'id': x, 'name': x, 'presentation': 'markdown'} if x == 'id' else {'id': x, 'name': x} for x in df_after_search.columns]
+    df_data = df_after_search_tabela.to_dict(orient='records')
+    df_columns = [{'id': x, 'name': x, 'presentation': 'markdown'} if x == 'id' else {'id': x, 'name': x} for x in df_after_search_tabela.columns]
     #df_columns = [{"name": i, "id": i} for i in df_after_search.columns]
+
+
+
+    df_after_search_cartoes = df_after_search.copy()
+
+    df_after_search_cartoes = df_after_search_cartoes.drop(
+        columns=[
+            'codigo',
+            'codigo_siars',
+            'nome_abreviado',
+            'formula',
+            'unidade_de_medida',
+            'output',
+            'area_clinica',
+            'tipo_de_indicador',
+            'estado_do_indicador',
+            'inclusao_de_utentes_no_indicador',
+            'prazo_para_registos',
+            'indexing',
+            'nome_indicador',
+        ]
+    )
+
+    cartoes_indicadores_gerados = [generate_html_indicador(i) for i in df_after_search_cartoes.iterrows()]
+
+
+
 
     # Texto que refere quantos indicadores estão na tabela resultado da pesquisa
     numero_indicadores_tabela = str(len(df_after_search))+' indicadores encontrados'
 
-    return df_data,df_columns,numero_indicadores_tabela
+    return df_data, df_columns, cartoes_indicadores_gerados, numero_indicadores_tabela,
 
