1586ed78465b0a9ce41737aa1889d9c050ab1e6a
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-11-12 14:55:05 +0000

    sunburst upgrades

--- pages/sunburst.py
+++ pages/sunburst.py
@@ -17,32 +17,32 @@ dash.register_page(
     order=3,
 )
 
-pythonanywhere_file_tree = ""
-# pythonanywhere_file_tree = '/home/diogocarapito/bi_indicadores/'
-df_sunburst = pd.read_csv(pythonanywhere_file_tree + "data/sunburst_data.csv")
+df_sunburst = pd.read_csv("data/sunburst_data_operacionalizado.csv")
 
+# lines where operacionalizar is false
+df_sunburst = df_sunburst[df_sunburst["operacionalizar"] == True]
 
-dropdown_options = ["unidade"]
+# create figure
+fig_sunburst_indicadores = go.Figure()
 
-filters = html.Div(
-    [
-        dbc.Row(
-            [
-                dbc.Col([], width=4),
-                dbc.Col(
-                    [
-                        dcc.Dropdown(
-                            options=dropdown_options,
-                            value=dropdown_options[0],
-                            id="dropdown_options",
-                        )
-                    ],
-                    width=4,
-                ),
-                dbc.Col([], width=4),
-            ]
-        )
-    ]
+fig_sunburst_indicadores.add_trace(
+    go.Sunburst(
+        ids=df_sunburst["name"],
+        labels=df_sunburst["label"],
+        parents=df_sunburst["parent"],
+        values=df_sunburst["value"],
+        #values=df_sunburst["valor_operacionalizado"],
+        branchvalues="total",
+        domain=dict(column=1),
+        insidetextorientation="radial",
+        hovertemplate="<b>%{label} </b> <br> peso: %{value:.2f}%",
+    )
+)
+
+fig_sunburst_indicadores.update_layout(
+    margin=dict(t=0, l=0, r=0, b=0),
+    width=800,
+    height=800,
 )
 
 
@@ -61,7 +61,7 @@ graphs = html.Div(
         dbc.Row(
             [
                 dbc.Col([], width=1),
-                dbc.Col([dcc.Graph(id="sunburst_indicadores")], width=10),
+                dbc.Col([dcc.Graph(figure=fig_sunburst_indicadores)], width=10),
                 dbc.Col([], width=1),
             ]
         ),
@@ -74,8 +74,6 @@ container = dbc.Container(
                 dbc.Col(
                     [
                         header,
-                        filters,
-                        html.Br(),
                         graphs,
                         html.Br(),
                     ]
@@ -94,94 +92,3 @@ def layout():
             html.Br(),
         ]
     )
-
-
-@callback(
-    Output("dropdown_options", "options"),
-    Input("store_data", "data"),
-)
-def stora_data_show(store_data):
-    if store_data == []:
-        return []
-    else:
-        df = pd.DataFrame(store_data)
-        # dropdown_list = ['unidade']
-        dropdown_list = []
-        # dropdown_list.extend(df['id_medico'].unique().tolist())
-        dropdown_list = df["id_medico"].unique().tolist()
-        return dropdown_list
-
-
-@callback(
-    Output("sunburst_indicadores", "figure"),
-    Input("dropdown_options", "value"),
-    Input("store_data", "data"),
-)
-def sunburst_update(dropdown_options_type, store_data):
-    if store_data != []:
-        df = pd.DataFrame(store_data)
-
-        df = df[df["id_medico"] == dropdown_options_type]
-        # df = df.dropna(subset='pontuacao')
-
-        # df_sunburst[df_sunburst['pontuacao'] == np.nan] = 1.1
-        # df_sunburst[df_sunburst['pontuacao_arredondada'] == np.nan] = 1.1
-        df_sunburst_with_score = df_sunburst.merge(df, on="id_indicador", how="outer")
-
-        # df_sunburst_with_score['pontuacao'] = df_sunburst_with_score['pontuacao'].fillna(0)
-
-        df.to_csv("data/df_from_upload.csv", index=True)
-        df_sunburst.to_csv("data/df_sunburst_pre_graph.csv", index=True)
-        df_sunburst_with_score.to_csv("data/df_sunburst_with_score.csv", index=True)
-
-    fig_sunburst_indicadores = go.Figure()
-
-    try:
-        if dropdown_options_type == "unidade":
-            cor = df_sunburst_with_score.pontuacao_arredondada
-        else:
-            cor = df_sunburst_with_score.pontuacao
-
-        fig_sunburst_indicadores.add_trace(
-            go.Sunburst(
-                ids=df_sunburst_with_score.id,
-                labels=df_sunburst_with_score.label,
-                parents=df_sunburst_with_score.parent,
-                values=df_sunburst_with_score.value,
-                branchvalues="total",
-                domain=dict(column=1),
-                insidetextorientation="radial",
-                marker=dict(
-                    colors=cor,
-                    cmax=2.8,
-                    cmin=-0.2,
-                    colorscale="temps",
-                    reversescale=True,
-                    # showscale=True,
-                ),
-                hovertemplate="<b>%{label} </b> <br> peso: %{value:.2f}%<br> score: %{color:.2f}",
-            )
-        )
-        print("YESS")
-    except (ValueError, KeyError) as e:
-        fig_sunburst_indicadores.add_trace(
-            go.Sunburst(
-                ids=df_sunburst.id,
-                labels=df_sunburst.label,
-                parents=df_sunburst.parent,
-                values=df_sunburst.value,
-                branchvalues="total",
-                domain=dict(column=1),
-                insidetextorientation="radial",
-                hovertemplate="<b>%{label} </b> <br> peso: %{value:.2f}%",
-            )
-        )
-        print(e)
-
-    fig_sunburst_indicadores.update_layout(
-        margin=dict(t=0, l=0, r=0, b=0),
-        width=800,
-        height=800,
-    )
-
-    return fig_sunburst_indicadores
