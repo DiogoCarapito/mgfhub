00beba0906a70077bd9789b029d378f236e1f41c
DiogoCarapito <diogo.carapito@icloud.com>
2024-01-05 20:00:56 +0000

    initial UI

--- mgfhub.py
+++ mgfhub.py
@@ -9,15 +9,7 @@ def main():
         initial_sidebar_state="expanded",
     )
 
-    with st.sidebar:
-        st.title("📄 Upload")
-        st.write("Upload your file here:")
-        uploaded_file = st.file_uploader("Choose a file", type="csv")
-        if uploaded_file is not None:
-            st.write("File uploaded successfully!")
-            st.write(uploaded_file)
-        else:
-            st.write("Please upload a file!")
+    
 
     st.title("mgfhub 2.0")
 
--- pages/3_📊_Sunburst.py
+++ pages/3_📊_Sunburst.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Sunburst")
+
+radio = st.radio(
+    "Modelo Contratual",
+    ["IDE", "IDG"],
+    horizontal=True
+)
