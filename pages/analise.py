509fdfd1f39f610d815dc534c0e018e0f03a71f3
DiogoCarapito <diogo.carapito@icloud.com>
2024-01-05 19:42:01 +0000

    analise.py

--- pages/analise.py
+++ pages/analise.py
@@ -1,5 +1,9 @@
 import dash
-from dash import html, dcc
+from dash import html, dcc, callback, Input, Output, State
+from dash.dependencies import Input, Output, State
+import base64
+import io
+import pandas as pd
 
 dash.register_page(
     __name__,
@@ -25,9 +29,8 @@ upload = html.Div(
                 "margin": "10px",
             },
             # Allow multiple files to be uploaded
-            multiple=True,
+            multiple=False,
         ),
-        html.Div(id="output-data-upload"),
     ]
 )
 
@@ -47,3 +50,27 @@ def layout():
             html.Br(),
         ]
     )
+
+@callback(
+    Output('store_data', 'data'),
+    Input('upload-data', 'contents'),
+    State('upload-data', 'filename')
+)
+def update_output(list_of_contents, list_of_names):
+    if list_of_contents is not None:
+        data = []
+        for content, name in zip(list_of_contents, list_of_names):
+            content_type, content_string = content.split(',')
+            decoded = base64.b64decode(content_string)
+            try:
+                if 'csv' in name:
+                    # Assume that the user uploaded a CSV file
+                    df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+                elif 'xls' in name:
+                    # Assume that the user uploaded an excel file
+                    df = pd.read_excel(io.BytesIO(decoded))
+                data.append(df.to_dict('records'))
+            except Exception as e:
+                print(e)
+        return data
+    return []
