171297e55a1598534b5dfdcfb185dcbebbf4292e
DiogoCarapito <diogo.carapito@icloud.com>
2024-04-16 00:29:17 +0100

    tutorial bug corrected where cache was givib render problems

--- utils/tutorial.py
+++ utils/tutorial.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-@st.cache_data()
+
 def tutorial_loop(tutorial):
     # loop through the content and create a card for each row
     for i, each in enumerate(tutorial):
@@ -14,7 +14,7 @@ def tutorial_loop(tutorial):
             st.write("")
 
 
-@st.cache_data()
+
 def tutorial_expander(tutorial):
     with st.expander("Tutorial", expanded=False):
         col_tutorial_1, col_tutorial_2, col_tutorial_3 = st.columns([1, 6, 1])
