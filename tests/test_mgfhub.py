3672b529fee9abc8ea5555e37b285555b260afb0
DiogoCarapito <diogo.carapito@icloud.com>
2024-04-28 12:48:30 +0100

    github actions test_mgfhub.py compatibility test

--- tests/test_mgfhub.py
+++ tests/test_mgfhub.py
@@ -1,14 +1,14 @@
-# from streamlit.testing.v1 import AppTest
+from streamlit.testing.v1 import AppTest
 
-# pages = [
-#     "mgfhub.py",
-#     "pages/2_Indicadores.py",
-#     "pages/3_IDE.py",
-#     "pages/5_FAQs.py",
-#     "pages/6_Sobre.py",
-# ]
+pages = [
+    "mgfhub.py",
+    "pages/2_Indicadores.py",
+    "pages/3_IDE.py",
+    "pages/5_FAQs.py",
+    "pages/6_Sobre.py",
+]
 
-# for each_page in pages:
-#     test = AppTest(each_page, default_timeout=30)
-#     test.run()
-#     assert not test.exception
+for each_page in pages:
+    test = AppTest(each_page, default_timeout=30)
+    test.run()
+    assert not test.exception
